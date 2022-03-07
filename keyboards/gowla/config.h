62137237b09e191c9d8f85fb18fbf886252719a9
David Poza Suárez <pozasuarez@gmail.com>
2022-03-07 20:10:31 +0100

    added config for 3x3 keyboard

--- keyboards/gowla/config.h
+++ keyboards/gowla/config.h
@@ -30,8 +30,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Pin-out */
 #define MATRIX_ROW_PINS { D1, D0, D4 }
-#define MATRIX_COL_PINS { B5, B4, E6 }
+#define MATRIX_COL_PINS { F5, F6, F7 }
 #define UNUSED_PINS
 
 /* ROW2COL */
-#define DIODE_DIRECTION ROW2COL
+#define DIODE_DIRECTION COL2ROW
