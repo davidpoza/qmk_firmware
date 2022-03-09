76b9e773346550dc17cc577935def37b9fe27410
David Poza Suárez <pozasuarez@gmail.com>
2022-03-09 17:52:32 +0100

    added passwords macros

--- keyboards/gowla/keymaps/default/keymap.c
+++ keyboards/gowla/keymaps/default/keymap.c
@@ -1,21 +1,24 @@
 #include QMK_KEYBOARD_H
 #include <sendstring_spanish.h>
+#include "passwords.h"
 
 enum custom_keycodes {
   WORDREFERENCE = SAFE_RANGE, // can always be here
-
+  PASSWORD1,
+  USUARIO1,
+  PASSWORD2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_3x3(
-        LT(1, KC_MPRV), KC_CALC, KC_MPLY,
+        KC_LEFT,        KC_CALC,         KC_MPLY,
         KC_VOLD,        WORDREFERENCE,   KC_VOLU,
-        KC_LEFT,        KC_DOWN, KC_RGHT
+        LT(1, KC_MPRV), KC_DOWN,         KC_ENT
     ),
     [1] = LAYOUT_ortho_3x3(
-        KC_ESC,  WORDREFERENCE, RESET,
-        KC_P7,   KC_P1,   KC_F1,
-        KC_F2,   KC_F3,   KC_F4
+        KC_F2,  KC_ESC, RESET,
+        USUARIO1,   PASSWORD1,   PASSWORD2,
+        KC_ESC,   KC_F3,   KC_F4
     )
 };
 
@@ -54,6 +57,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     SEND_STRING(SS_TAP(X_ENTER));
                 } else {
                     // when keycode is released
+                }
+                return true;
+        case USUARIO1:
+                if (record->event.pressed) {
+                    SEND_STRING(USER1);
+                }
+                return true;
+        case PASSWORD1:
+                if (record->event.pressed) {
+                    SEND_STRING(PASS1);
+                }
+                return true;
+        case PASSWORD2:
+                if (record->event.pressed) {
+                    SEND_STRING(PASS2);
                 }
                 return true;
         default:
